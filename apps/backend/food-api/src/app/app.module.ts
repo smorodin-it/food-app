c255db2dead93914c731ac3f0efd5f0e005ed9e7
Smorodin I.T <smorodin.it@gmail.com>
2023-03-26 21:18:17 +0300

    fix import

--- apps/backend/food-api/src/app/app.module.ts
+++ apps/backend/food-api/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BackendOrmModule } from '@food-app/backend/orm';
+import { BackendUserModule } from '@food-app/backend/user';
 
 @Module({
-  imports: [BackendOrmModule],
+  imports: [BackendOrmModule, BackendUserModule],
   controllers: [AppController],
   providers: [AppService],
 })
