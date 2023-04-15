0a513e8f8b15d8abc1279e8395fe043647f26725
Smorodin I.T <smorodin.it@gmail.com>
2023-04-15 12:33:27 +0300

    validation

--- libs/backend/auth/src/lib/backend-auth.decorators.ts
+++ libs/backend/auth/src/lib/backend-auth.decorators.ts
@@ -1,4 +1,38 @@
 import { SetMetadata } from '@nestjs/common';
 import { IS_PUBLIC_KEY } from './backend-auth.constants';
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+} from 'class-validator';
 
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+export function IsEqualTo<T>(
+  property: keyof T,
+  validationOptions?: ValidationOptions
+) {
+  return (object: Record<string, unknown>, propertyName: string) => {
+    registerDecorator({
+      name: 'isEqualTo',
+      target: object.constructor,
+      propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        validate(value: Record<string, unknown>, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as Record<string, unknown>)[
+            relatedPropertyName
+          ];
+          return value === relatedValue;
+        },
+
+        defaultMessage(args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          return `${propertyName} must match ${relatedPropertyName} exactly`;
+        },
+      },
+    });
+  };
+}
--- libs/backend/auth/src/lib/backend-auth.dto.ts
+++ libs/backend/auth/src/lib/backend-auth.dto.ts
@@ -1,11 +1,23 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEqualTo } from './backend-auth.decorators';
+
 export class SignInDto {
+  @IsEmail()
   email!: string;
+
+  @IsNotEmpty()
   password!: string;
 }
 
 export class SignUpDto {
+  @IsEmail()
   email!: string;
+
+  @IsNotEmpty()
   password!: string;
+
+  @IsNotEmpty()
+  @IsEqualTo<SignUpDto>('password')
   confirmPassword!: string;
 }
 
