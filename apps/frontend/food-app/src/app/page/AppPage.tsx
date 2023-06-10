c4b6ae2621967b3163bb777348b407be2bd8e6d2
Smorodin I.T <smorodin.it@gmail.com>
2023-06-10 15:57:05 +0300

    remove test functionality

--- apps/frontend/food-app/src/app/page/AppPage.tsx
+++ apps/frontend/food-app/src/app/page/AppPage.tsx
@@ -1,43 +1,27 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import { AppLayout } from '../layout/AppLayout/AppLayout';
 import { AppSideBarLayout } from '../layout/AppLayout/AppSideBarLayout';
 import { AppTopBarLayout } from '../layout/AppLayout/AppTopBarLayout';
 import { AppAreaLayout } from '../layout/AppLayout/AppAreaLayout';
-import { Button } from '@mui/material';
-import { $api } from '@food-app/frontend/data-access/https';
 import { useWindowSize } from '@food-app/frontend/utils';
 import { MediaMinWidthConstants } from '@food-app/frontend/ui';
 
 export const AppPage: FC = () => {
-  const [data, setData] = useState<any[]>([]);
   const { width } = useWindowSize();
-
-  const handleClick = async (): Promise<void> => {
-    try {
-      const resp = await $api.get('/ingredient');
-
-      if (resp) {
-        setData(resp.data);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
 
   return (
     <AppLayout>
+      {/* Show sidebar if screen gte laptop */}
       {width >= MediaMinWidthConstants.laptop && (
         <AppSideBarLayout>Sidebar</AppSideBarLayout>
       )}
+      {/* Show topbar if screen lt laptop */}
       {width < MediaMinWidthConstants.laptop && (
         <AppTopBarLayout>Top bar</AppTopBarLayout>
       )}
       <AppAreaLayout>
         <Outlet />
-        <Button onClick={handleClick}>Get data</Button>
-
-        <pre>{JSON.stringify(data, undefined, 2)}</pre>
       </AppAreaLayout>
     </AppLayout>
   );
