b2489bf8100eb0edc064a0561e731172c906cbc0
smorodin-it <81906452+smorodin-it@users.noreply.github.com>
2023-06-11 15:12:19 +0300

    implement custom breakpoints (#14)
    implement usage media query from mui theme breakpoints

--- apps/frontend/food-app/src/app/theme/theme.ts
+++ apps/frontend/food-app/src/app/theme/theme.ts
@@ -1,6 +1,29 @@
 import { createTheme } from '@mui/material';
 
+declare module '@mui/material/styles' {
+  interface BreakpointOverrides {
+    xs: false; // removes the `xs` breakpoint
+    sm: false;
+    md: false;
+    lg: false;
+    xl: false;
+    mobile: true; // adds the `mobile` breakpoint
+    tablet: true;
+    laptop: true;
+    desktop: true;
+  }
+}
+
 export const theme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 480,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
+
   typography: {
     htmlFontSize: 10,
   },
@@ -9,6 +32,12 @@ export const theme = createTheme({
       styleOverrides: {
         html: {
           fontSize: '10px',
+        },
+        '#root': {
+          minWidth: 280,
+          maxWidth: 1200,
+          width: '100%',
+          margin: '0 auto',
         },
       },
     },
