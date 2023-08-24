c21b3c1a67ad8c4e8acf1408662880ec99a6782b
Smorodin I.T <smorodin.it@gmail.com>
2023-08-24 12:41:43 +0300

    move ingredient validation to zod schemas

--- libs/backend/features/ingredient/src/lib/backend-ingredient.dto.ts
+++ libs/backend/features/ingredient/src/lib/backend-ingredient.dto.ts
@@ -1,13 +1,22 @@
-export class IngredientCreateUpdateDto {
-  name!: string;
-  manufacturer!: string;
-  barcode!: string;
-  proteins!: number;
-  carbs!: number;
-  fats!: number;
-  calories!: number;
-}
+import { z } from 'nestjs-zod/z';
+import { createZodDto } from 'nestjs-zod/';
 
-export class IsDeletedDto {
-  isDeleted!: boolean;
-}
+export const IngredientCreateUpdateSchema = z.object({
+  name: z.string(),
+  manufacturer: z.string(),
+  barcode: z.string(),
+  proteins: z.number(),
+  carbs: z.number(),
+  fats: z.number(),
+  calories: z.number(),
+});
+
+export class IngredientCreateUpdateDto extends createZodDto(
+  IngredientCreateUpdateSchema
+) {}
+
+export const IsDeletedSchema = z.object({
+  isDeleted: z.boolean(),
+});
+
+export class IsDeletedDto extends createZodDto(IsDeletedSchema) {}
