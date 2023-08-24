a0d000cc8b015d66870cb3ed69dbc5981fa5e571
Smorodin I.T <smorodin.it@gmail.com>
2023-08-24 13:04:16 +0300

    fix typo

--- libs/backend/core/src/lib/core-dto/QueryDto.ts
+++ libs/backend/core/src/lib/core-dto/QueryDto.ts
@@ -4,7 +4,7 @@ import { createZodDto } from 'nestjs-zod';
 
 export const PaginationQuerySchema = z.object({
   page: z.number().positive().default(1),
-  pePage: z.number().gte(0).default(BackendCoreConstants.DefaultPerPage),
+  perPage: z.number().gte(0).default(BackendCoreConstants.DefaultPerPage),
 });
 
 export class PaginationQueryDto extends createZodDto(PaginationQuerySchema) {}
